94b65fc4620d414d7a3cca9a49113e3b94e7223f
Zachary Allin <zachary.allin@gartner.com>
2018-01-31 10:27:14 -0500

    moved styles, setting up webpack and deployment

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import Button from 'src/components/buttons/Button';
+
+import * as datetimeStyles from 'react-datetime/css/react-datetime.css';
+import styles from 'src/styles/master.scss';
+
+module.exports = {
+    Button, 
+};
---
+++ webpack.config.js
@@ -0,0 +1,89 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var setupVariables = function() {
+    switch(process.env.NODE_ENV) {
+        case 'production': 
+           
+            break;
+        case 'qa':
+           
+            break;
+        case 'dev':
+           
+            break;
+        default:
+           
+            break;
+    }
+};
+setupVariables();
+
+module.exports = {
+    entry: [
+        'react-hot-loader/patch', 
+        './src/index.js'
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+        compress: true, 
+        port: 9000, 
+        hot: true
+    }, 
+    plugins: [
+        new webpack.NamedModulesPlugin(), 
+        new webpack.HotModuleReplacementPlugin(), 
+        new webpack.DefinePlugin({
+        }), 
+        new ExtractTextPlugin("styles.css")
+    ],
+    module: {
+        rules: [
+            { 
+                test: /\.jsx?/, 
+                exclude: /node_modules/, 
+                loader: 'babel-loader' 
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader', 
+                    use: [
+                        {
+                            loader: 'css-loader', 
+                            options: {
+                                minimize: true, 
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader', 
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            },
+            {
+                test: /\.(woff2?|ttf|otf|eot|svg)$/, 
+                exclude: /node_modules/, 
+                loader: 'url-loader?limit=100000'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json'], 
+        modules: [
+            path.resolve(__dirname, './'), 
+            path.resolve(__dirname, 'node_modules')
+        ]
+    }, 
+    output: {
+        filename: 'index.js', 
+        path: path.resolve(__dirname, './dist'), 
+        publicPath: '/dist'
+    }
+
+};
